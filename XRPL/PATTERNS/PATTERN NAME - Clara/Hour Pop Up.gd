8c31e4f2abec79ec8614bf9e025e6b1dd93215c1
claralevin <clevin1@sva.edu>
2024-03-25 16:12:01 -0400

    updates

--- XRPL/PATTERNS/PATTERN NAME - Clara/Hour Pop Up.gd
+++ XRPL/PATTERNS/PATTERN NAME - Clara/Hour Pop Up.gd
@@ -1,23 +1,23 @@
 extends Node3D
-@onready var hourselapsed = $"hours-elapsed"
-var timepassed = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		ui_left()
-	$Label3D.text=str(Time.get_ticks_msec()/60000)
-	timepassed = Time.get_ticks_msec()
-	if timepassed > 3000:
-		show()
-		
-	var currenttime = Time.get_datetime_dict_from_system()
-	print(currenttime.second) 
-	
-func ui_left():
-	$"../AnimationPlayer".play("spin")
+#@onready var hourselapsed = $"hours-elapsed"
+#var timepassed = 0
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#ui_left()
+	#$Label3D.text=str(Time.get_ticks_msec()/60000)
+	#timepassed = Time.get_ticks_msec()
+	#if timepassed > 3000:
+		#show()
+		#
+	#var currenttime = Time.get_datetime_dict_from_system()
+	#print(currenttime.second) 
+	#
+#func ui_left():
+	#$"../AnimationPlayer".play("spin")
