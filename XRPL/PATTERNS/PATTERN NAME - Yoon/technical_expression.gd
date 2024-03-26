48e9208dc647d72d6f11cb3fa6638c12c73f1b8f
jnnfrdesign <jcai8@sva.edu>
2024-03-26 18:23:19 -0400

    updates

---
+++ XRPL/PATTERNS/PATTERN NAME - Yoon/technical_expression.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_timer_timeout():
+	var newpoop=$Poop.duplicate()
+	newpoop.position=$XROrigin3D/XRCamera3D.position
+	add_child(newpoop)
+	newpoop.position.z=randf_range(0,0.5)
