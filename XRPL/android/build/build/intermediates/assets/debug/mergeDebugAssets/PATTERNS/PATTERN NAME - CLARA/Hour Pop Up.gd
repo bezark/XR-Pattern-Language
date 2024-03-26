47f12c4cc739548e6e6eb4a86006618a8afd5c21
bezark <jdb459@nyu.edu>
2024-03-26 14:31:57 -0700

    S**t together

--- XRPL/android/build/build/intermediates/assets/debug/mergeDebugAssets/PATTERNS/PATTERN NAME - CLARA/Hour Pop Up.gd
+++ XRPL/android/build/build/intermediates/assets/debug/mergeDebugAssets/PATTERNS/PATTERN NAME - CLARA/Hour Pop Up.gd
@@ -1,23 +1,36 @@
 extends Node3D
-@onready var hourselapsed = $"hours-elapsed"
-var timepassed = 0
+#@onready var hourselapsed = $"hours-elapsed"
+#var timepassed = 0
+#
+## Called when the node enters the scene tree for the first time.
+#func _ready():
+	#pass # Replace with function body.
+#
+#
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#ui_left()
+	#$Label3D.text=str(Time.get_ticks_msec()/60000)
+	#timepassed = Time.get_ticks_msec()
+	#if timepassed > 3000:
+		#show()
+		#
+	#var currenttime = Time.get_datetime_dict_from_system()
+	#print(currenttime.second) 
+	#
+#func ui_left():
+	#$"../AnimationPlayer".play("spin")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _on_area_3d_area_entered(area):
+	hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		ui_left()
-	$Label3D.text=str(Time.get_ticks_msec()/60000)
-	timepassed = Time.get_ticks_msec()
-	if timepassed > 3000:
-		show()
-		
-	var currenttime = Time.get_datetime_dict_from_system()
-	print(currenttime.second) 
-	
-func ui_left():
+func _on_breakarea_area_entered(area):
+	hide()
+	$message.show()
+
+
+func _on_next_area_entered(area):
 	$"../AnimationPlayer".play("spin")
