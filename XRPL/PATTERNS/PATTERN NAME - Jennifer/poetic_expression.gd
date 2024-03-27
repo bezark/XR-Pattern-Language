5fb27ed0debc9b2bd1bd8984a1745fd7835bab2a
bezark <johnbezark@gmail.com>
2024-03-27 17:43:23 -0400

    Jennifer

---
+++ XRPL/PATTERNS/PATTERN NAME - Jennifer/poetic_expression.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#$CharacterBody3D/XROrigin3D/XRCamera3D.position = Vector3(-0.14, -7.031, 3.749)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
